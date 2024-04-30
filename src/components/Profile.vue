b79cc662d1b8df533a7c9077780eb9b0c791c9c1
tofaade18 <tofa.ade@bithealth.co.id>
2024-04-30 12:35:58 +0700

    frontend baru

---
+++ src/components/Profile.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container">
+    <header class="jumbotron mt-4">
+      <h2>Profile</h2>
+      <h3>
+        <strong>{{currentUser.username}}</strong>
+      </h3>
+    </header>
+    <p>
+      <strong>Id:</strong>
+      {{currentUser.id}}
+    </p>
+    <p>
+      <strong>Email:</strong>
+      {{currentUser.email}}
+    </p>
+    <div class="history mt-5">
+      <h3>History</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Profile',
+  methods: {
+    cekparam() {
+    // console.log(req.params.id)
+  }
+},
+  computed: {
+    currentUser() {
+      return this.$store.state.auth.user;
+    }
+  },
+  mounted() {
+    // this.cekparam()
+    if (!this.currentUser) {
+      this.$router.push('/login');
+    }
+  }
+};
+</script>
